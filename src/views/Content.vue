97baa4ba095ebb81104899772a34278e5ccea2c3
Presab <presabkhadka30@gmail.com>
2025-07-05 15:16:19 +0545

    updt- content page

--- src/views/Content.vue
+++ src/views/Content.vue
@@ -7,8 +7,14 @@ let contents = ref([]);
 
 let fetchContents = async () => {
   try {
+    let token = localStorage.getItem("Authorization");
     let response = await axios.get(
-      `${import.meta.env.VITE_BASE_URL}/api/content/fetch`
+      `${import.meta.env.VITE_BASE_URL}/api/admin/total-content`,
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
     );
     contents.value = response.data.contents;
   } catch (error) {
@@ -23,6 +29,7 @@ let publishContent = async (id) => {
     let token = localStorage.getItem("Authorization");
     await axios.patch(
       `${import.meta.env.VITE_BASE_URL}/api/admin/content/publish/${id}`,
+      {},
       {
         headers: {
           Authorization: token,
@@ -33,6 +40,8 @@ let publishContent = async (id) => {
     if (error instanceof Error) {
       console.log("Error", error.message);
     }
+  } finally {
+    fetchContents();
   }
 };
 
@@ -41,6 +50,7 @@ let draftContent = async (id) => {
     let token = localStorage.getItem("Authorization");
     await axios.patch(
       `${import.meta.env.VITE_BASE_URL}/api/admin/content/draft/${id}`,
+      {},
       {
         headers: {
           Authorization: token,
@@ -51,6 +61,28 @@ let draftContent = async (id) => {
     if (error instanceof Error) {
       console.log("Error", error.message);
     }
+  } finally {
+    fetchContents();
+  }
+};
+
+let deleteContent = async (id) => {
+  try {
+    let token = localStorage.getItem("Authorization");
+    await axios.delete(
+      `${import.meta.env.VITE_BASE_URL}/api/admin/content/delete/${id}`,
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log("Error", error.message);
+    }
+  } finally {
+    fetchContents();
   }
 };
 
@@ -70,6 +102,7 @@ onMounted(() => {
           <tr>
             <th class="text-left px-4 py-2">title</th>
             <th class="text-left px-4 py-2">status</th>
+            <th class="text-left px-4 py-2">slug</th>
             <th class="text-left px-4 py-2">actions</th>
           </tr>
         </thead>
@@ -77,10 +110,11 @@ onMounted(() => {
           <tr v-for="(cont, index) in contents" :key="index">
             <td class="px-4 py-2">{{ cont.title }}</td>
             <td class="px-4 py-2">{{ cont.status }}</td>
-            <td class="px-4 py-2">
+            <td class="px-4 py-2">{{ cont.slug }}</td>
+            <td class="px-4 py-2 flex gap-2">
               <button
                 v-if="cont.status !== 'PUBLISHED'"
-                class="px-4 py-2 rounded-lg bg-green-500 text-white"
+                class="px-4 py-2 rounded-lg bg-green-500 text-white hover:cursor-pointer hover:bg-green-300"
                 @click="publishContent(cont.id)"
               >
                 Publish this content
@@ -91,6 +125,12 @@ onMounted(() => {
                 @click="draftContent(cont.id)"
               >
                 Draft this content
+              </button>
+              <button
+                class="px-4 py-2 rounded-lg bg-red-500 hover:cursor-pointer hover:bg-red-300 text-white"
+                @click="deleteContent(cont.id)"
+              >
+                Delete
               </button>
             </td>
           </tr>
